ee70d53404397abee9fd542c5878382c9dc4e39c
Rogerio-N <rogenakayama@gmail.com>
2021-06-12 21:54:02 -0400

    Added put method integration

---
+++ JS/edit-data.js
@@ -0,0 +1,42 @@
+function put(url,data){
+    let request = new XMLHttpRequest();
+    request.open("PUT",url,true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(JSON.stringify(data));
+    return request.responseText;
+}
+
+function updateData(){
+    event.preventDefault();
+    let currentUserId = parseInt(sessionStorage.getItem("Id"));
+    let url = "http://localhost:8080/users/"+currentUserId;
+
+    let newName = document.getElementById("namee").value;
+    let newEmail = document.getElementById("emaill").value;
+    
+    let password = document.getElementById("senhaa").value;
+    let confPassword = document.getElementById("confsenhaa").value;
+
+    var canCreate = false;
+    var newPassword = "";
+
+    if(password == confPassword){
+        console.log("Aqui")
+        canCreate = true;
+        newPassword = confPassword;
+    }
+
+    if (canCreate){
+        const data = {
+            "name":newName,
+            "email":newEmail,
+            "password":newPassword
+        }
+        put(url,data);
+        window.location.href = "./home.html"
+    }else{
+        alert("Algo deu errado com sua requisicao");
+    }
+
+    
+}
