cd094a5143272fe70b7fcf79f97582d8f75f8a25
Rogerio-N <rogenakayama@gmail.com>
2021-06-22 20:09:58 -0400

    [FIX] Added clear querry button and changed querry logic

--- JS/home.js
+++ JS/home.js
@@ -31,6 +31,8 @@ function complaintInsert(){
         
         if(element.user.id == currentUser){
             let row = document.createElement("tr");
+            row.className = "allUserComplaint";
+            row.id = element.protocol;
 
             let dateSend = document.createElement("td");
             let dataSendPrep = new Date(element.dataEnvio);
@@ -79,5 +81,80 @@ function complaintInsert(){
 
 }
 
-
 complaintInsert();
+
+function searchComplaint(){
+    let currentComplaintProtocol = document.getElementById("Protocol-text-inpt").value;
+
+    let rawData = get("https://obras-publicas.herokuapp.com/complaint/protocolSearch?protocol="+currentComplaintProtocol);
+    let Complaint = JSON.parse(rawData);
+
+    if(Complaint.length!=0){
+
+            let table = document.getElementById("History-table");
+            let oldDivs = document.getElementsByClassName("allUserComplaint");
+            
+    
+            for(let i = 0; i<oldDivs.length;i++){
+                oldDivs[i].style.display = "none";
+            }
+            
+                let row = document.createElement("tr");
+                row.className = "currentComplaint";
+    
+                let dateSend = document.createElement("td");
+                let dataSendPrep = new Date(Complaint[0].dataEnvio);
+                let dataSendDay = dataSendPrep.getDate().toString();
+                let dataSendMonth = dataSendPrep.getMonth().toString();
+                let dataSendYear = dataSendPrep.getFullYear().toString();
+                let fullSendDate = dataSendDay+"/"+dataSendMonth+"/"+dataSendYear;
+                dateSend.innerHTML = fullSendDate;
+        
+        
+                let protocol = document.createElement("td");
+                protocol.innerHTML = Complaint[0].protocol;
+        
+                let org = document.createElement("td");
+                org.innerHTML = "SISEP"
+        
+                let dateEnd = document.createElement("td");
+                let dataEndPrep = new Date(Complaint[0].dataFim);
+                let dataEndDay = dataEndPrep.getDate().toString();
+                let dataEndMonth = dataEndPrep.getMonth().toString();
+                let dataEndYear = dataEndPrep.getFullYear().toString();
+                let fullEndDate = dataEndDay+"/"+dataEndMonth+"/"+dataEndYear;
+                dateEnd.innerHTML = fullEndDate;
+        
+                let status = document.createElement("td");
+                status.innerHTML = Complaint[0].status;
+        
+                row.appendChild(dateSend);
+                row.appendChild(protocol);
+                row.appendChild(org);
+                row.appendChild(dateEnd);
+                row.appendChild(status);
+        
+                table.appendChild(row);
+
+
+    }else{
+        alert("Nenhuma denuncia encontrada com esse numero de protocolo");
+    }
+
+}
+
+function cleanQuerry(){
+
+    let search = document.getElementsByClassName("currentComplaint");
+
+    for(let i = 0; i<search.length;i++){
+        search[i].style.display = "none";
+    }
+
+    let oldDivs = document.getElementsByClassName("allUserComplaint");
+
+        for(let i = 0; i<oldDivs.length;i++){
+            oldDivs[i].style.display = "table-row";
+        }
+
+}
